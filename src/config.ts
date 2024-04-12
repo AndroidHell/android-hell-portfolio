ddc24efcd7dcc117acac125ea07c06eaf9de809e
AndroidHell <ralkkai1337@gmail.com>
2024-04-12 12:55:45 -0500

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE =
+  "Derrick's Portfolio | Websites and Web Apps that actually work for you";
+export const SITE_DESCRIPTION =
+  "This is my personal portfolio. I am a freelance web developer, specializing in efficient and snappy static websites built with AstroJS. I make awesome websites for your businesses that are fast, responsive, and focused on world class performance and top-notch SEO rating. This means you will always have a top spot on Google searches.";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
